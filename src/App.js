681ab0bcd8c967f3094bc968179fd0bb5e47dd70
unknown <chode8703@gmail.com>
2020-03-29 14:04:49 +0900

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
   // state : change datas
@@ -21,7 +20,18 @@ class App extends React.Component {
     // this.setState({count: this.state.count-1})
     this.setState(current => ({count: current.count-1}));
   };
+  // componentDidMount : "이 component는 처음 render됐어."
+  componentDidMount() {
+    console.log("component rendered")
+  }
+  componentDidUpdate() {
+    console.log("I just updated")
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world")
+  }
   render() {
+    console.log("I'm rendering")
     return <div>
       <h1>The number is: {this.state.count}</h1>
       <button onClick={this.add}>Add</button>
