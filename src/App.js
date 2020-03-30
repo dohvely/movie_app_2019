c42b55cab88e7eadf6f41e456bbed8684def92b0
unknown <chode8703@gmail.com>
2020-03-30 21:14:21 +0900

    axios, javascript ES6 비동기('async', 'await')

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  // state : change datas
   state = {
-    count : 0
+    isLoading: true,
+    movies: []
   }
-  add = () => {
-    console.log("add");
-    // nope! 이렇게 하면 React에서 render function을 refresh 하지 않기 떄문에 setState()를 통해 변경해야 한다.
-    // * React는 setState()가 실행될때마다 chage된 state와 함께 render function을 호출한다!
-    // this.state.count = 1;
-    // this.state.count로 직접 state를 사용하는 것은 좋은 코딩은 아니다.
-    // this.setState({count: this.state.count+1})
-    this.setState(current => ({count: current.count+1}));
-  };
-  minus = () => {
-    console.log("minus");
-    // this.state.count = -1;
-    // this.setState({count: this.state.count-1})
-    this.setState(current => ({count: current.count-1}));
-  };
-  // componentDidMount : "이 component는 처음 render됐어."
+  // 비동기
+  getMovies = async() => {
+    // await 은 async가 있을때만 쓰인다.
+    // async = 이건 좀 시간이 걸리니 기다려줘.
+    // 무엇을?
+    // await이 앞에있는 axios를.
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
+  } 
   componentDidMount() {
-    console.log("component rendered")
-  }
-  componentDidUpdate() {
-    console.log("I just updated")
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world")
+    setTimeout(() => {
+      // this.setState({isLoading: false})
+      this.getMovies();
+    }, 6000)
   }
   render() {
-    console.log("I'm rendering")
+    const {isLoading} = this.state
     return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
   }
 }
