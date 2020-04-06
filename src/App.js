9ff90102aae18a46335c79cb75cacdcb38592723
unknown <chode8703@gmail.com>
2020-04-06 21:27:51 +0900

    router 준비, react-router-dom 설치

--- src/App.js
+++ src/App.js
@@ -1,60 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie'
-import './App.css'
 
-// react hook 을 사용한다면 state를 사용하기 위해 class component를 사용하지 않아도 된다.
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  }
-  // 비동기
-  getMovies = async() => {
-    // await 은 async가 있을때만 쓰인다.
-    // async = 이건 좀 시간이 걸리니 기다려줘.
-    // 무엇을?
-    // await이 앞에있는 axios를.
-    const {
-      data: {
-        data: {movies}
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
-    // console.log(movies)
-    
-    // 같은 코드 : this.setState({movies}) = this.setState({movies: movies})
-    this.setState({movies, isLoading: false})
-  } 
-  componentDidMount() {
-    // setTimeout(() => {
-      // this.setState({isLoading: false})
-      this.getMovies();
-    // }, 6000)
-  }
-  render() {
-    const {isLoading, movies} = this.state
-    return <section className="container">
-      {isLoading
-      ?
-      <div className="loader">
-        <span className="loader__text">Loading..</span>
-      </div>
-      :
-      <div className="movies">
-        {movies.map(movie => {
-          return <Movie key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                  />
-        })}  
-      </div>
-      }
-    </section>
-  }
+function App() {
+  return <span>something</span>
 }
 
-export default App;
+export default App
