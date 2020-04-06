9c085c189cb3d410de165eccfb6e9f2c56b9c972
unknown <chode8703@gmail.com>
2020-04-06 22:07:07 +0900

    HashRouter, Router, Link (Navigation 컴포넌트)

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navigation() {
+  // Link는 Router 안에서 쓸 수 있다. (App.js에서 이 Navigation component를 import하여 Router 안에서 사용하고 있음)
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+export default Navigation
